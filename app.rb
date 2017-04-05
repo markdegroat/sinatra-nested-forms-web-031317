491773ee6a22ad9260a4080f0d61891e9c7203d1
markdegroat <mhdegroat@gmail.com>
2017-04-05 17:41:13 -0400

    Final

--- app.rb
+++ app.rb
@@ -3,7 +3,26 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :"/pirates/new"
+    end
+
+    get '/new' do
+      erb :"/pirates/new"
+    end
+
+
+
     # code other routes/actions here
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |ship_info|
+        Ship.new(ship_info)
+      end
+      @ships = Ship.all
+
+      erb :"/pirates/show"
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = args[:weight]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+
 end
